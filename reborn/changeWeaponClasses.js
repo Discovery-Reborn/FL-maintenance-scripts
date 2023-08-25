ecbb215c7845aadab317ac98b1d0b3e4c05d8265
Polarts <davidklein.4496@gmail.com>
2023-08-25 14:26:02 +0300

    Updated replacers

--- reborn/changeWeaponClasses.js
+++ reborn/changeWeaponClasses.js
@@ -4,13 +4,12 @@ const classMap = {
     "hp_gun_special_2": "hp_gun_special_7",
     "hp_gun_special_3": "hp_gun_special_10",
     "hp_gun_special_4": "hp_gun_special_8",
-    "hp_gun_special_5": "hp_gun_special_9", // Transport Turret (Heavy) not mentioned in new mapping
+    "hp_gun_special_5": "hp_turret_special_3",
     "hp_gun_special_6": "hp_gun_special_1",
     "hp_gun_special_7": "hp_gun_special_9",
     "hp_gun_special_8": "hp_gun_special_3",
     "hp_gun_special_9": "hp_gun_special_6",
     "hp_gun_special_10": "hp_gun_special_10",
-    // Missing: Long Range CD for LF only, HF/VHF CDs, Special Guns (i.e missiles, maxim gun, etc)
 }
 
 const fs = require("fs");
