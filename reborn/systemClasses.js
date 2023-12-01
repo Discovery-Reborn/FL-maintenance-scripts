e00913ff70d962e5d05f4b627a97f3bc54fbf9af
Polarts <davidklein.4496@gmail.com>
2023-12-01 18:42:30 +0200

    first version of complete script

--- reborn/systemClasses.js
+++ reborn/systemClasses.js
@@ -1,11 +1,27 @@
+const { Section, Entry } = require("bini-lib");
+
+/*
+interface Enncounter {
+    type: string
+    level: number
+    chance: number
+    factions: Faction[]
+}
+
+interface Faction {
+    nickname: string
+    density: number
+}
+*/
+
 function applySectionValues(obj, entries) {
     for (const entry of entries) {
         let entryObj = {}
-        if (entry.values.length === 1) {
-            entryObj[entry.name] = entry.values[0];
-        } else {
+        // if (entry.values.length === 1) {
+        //     entryObj[entry.name] = entry.values[0];
+        // } else {
             entryObj[entry.name] = entry.values;
-        }
+        // }
         Object.assign(obj, entryObj);
     }
 }
@@ -15,11 +31,70 @@ class Zone {
         if (section.name === "zone") {
             const nonEncounters = section.entries.filter(e => e.name !== "encounter" && e.name !== "faction");
             applySectionValues(this, nonEncounters);
-            // TODO add encounters
+            this.encounters = [];
+            let currentEncounter = {};
+            section.entries.forEach(entry => {
+                if (entry.name === "encounter") {
+                    if (currentEncounter !== {}) {
+                        this.encounters.push(currentEncounter);
+                    }
+                    currentEncounter = {
+                        type: entry.values[0],
+                        level: entry.values[1],
+                        chance: entry.values[2],
+                        factions: []
+                    }
+                }
+                if (entry.name === "faction") {
+                    currentEncounter.factions.push([{
+                        nickname: entry.values[0],
+                        density: entry.values[1]
+                    }]);
+                }
+            });
+            if (currentEncounter !== {}) {
+                this.encounters.push(currentEncounter);
+            }
         }
+    }
+
+    toSection() {
+        const section = new Section("zone");
+        const entries = Object.entries(this);
+        entries.forEach(([key, value]) => {
+            if (key === "encounters") {
+                value.forEach(encounter => {
+                    section.entries.push(new Entry("encounter", [encounter.type, encounter.level, encounter.chance]));
+                    encounter.factions.forEach(fac => {
+                        section.entries.push(new Entry("faction", [fac.nickname, fac.density]));
+                    });
+                })
+            } else {
+                section.entries.push(new Entry(key, value));
+            }
+        });
+        return section;
+    }
+
+    toString() {
+        let str = "[zone]\n";
+        const entries = Object.entries(this);
+        entries.forEach(([key, value]) => {
+            if (key === "encounters") {
+                value.forEach(encounter => {
+                    str += "encounter = " + encounter.type + ", " + encounter.level + ", " + encounter.chance + "\n";
+                    encounter.factions.forEach(fac => {
+                        str += "faction = " + fac.nickname + ", " + fac.density + "\n";
+                    });
+                })
+            } else {
+                str += key + " = " + value.join(", ") + "\n";
+            }
+        });
+        return str;
     }
 }
 
-class Encounter {
-
+module.exports = {
+    Zone
 }
--- reborn/updateFactionLevels.js
+++ reborn/updateFactionLevels.js
@@ -1,6 +1,18 @@
 const { getSystemFiles, getSectionsFromIni } = require('../functions');
 const fsAsync = require('fs').promises;
 const { IniFile } = require('bini-lib');
+const { Zone } = require('./SystemClasses');
+
+const specials = {
+    "gd_bh_grp": 18,
+    "fc_h_grp": 18,
+    "fc_corse_grp": 18,
+    "fc_j_grp": 18,
+    "gd_z_grp": 18,
+    "fc_freelancer": 18
+}
+
+const specialKeys = Object.keys(specials);
 
 async function updateFactionLevels() {
     const systemFiles = await getSystemFiles(['intro', 'iw09']);
@@ -11,7 +23,30 @@ async function updateFactionLevels() {
         if (fileContent) {
             const sysIni = IniFile.fromString(file, fileContent);
             const zones = sysIni.sections.filter(s => s.name === "zone");
-            
+            for (let zoneSection of zones) {
+                const zone = new Zone(zoneSection);
+                zone.encounters.forEach(enc => {
+                    const factionNames = enc.factions.map(f => f.nickname);
+                    const relFactionProps = factionProps.filter(f => factionNames.includes(f.affiliation));
+                    for (const fp of relFactionProps) {
+                        if (specialKeys.includes(fp.affiliation)) {
+                            enc.level = specials[fp.affiliation];
+                            break;
+                        }
+                        if (fp.legality === "lawful") {
+                            enc.level = 17;
+                            break;
+                        }
+                        if (fp.legality === "unlawful") {
+                            enc.level = 19;
+                            break;
+                        }
+                    }
+                })
+                zoneSection = zone.toSection();
+            }
+            const fileString = sysIni.toString();
+            await fsAsync.writeFile("res_"+file, fileString);
         }
     }
 }
